e762babbdfa5f98f4fb6dda61629cc9257fda107
Brian <github@easyecho.com>
2019-02-06 14:37:32 -0500

    Add basic terraform code

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = "us-east-1"
+  assume_role {
+    role_arn     = "arn:aws:iam::399378614282:role/terraform"
+    session_name = "terraform"
+  }
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "csa-demo-bucket"
+  acl    = "public-read"
+}
+
